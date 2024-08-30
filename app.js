5e3d3e7b4fb2c660e770ea10b19b11c458ebdb74
ThayseAda <thayse.frankenberger@gmail.com>
2024-08-30 19:36:33 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+function farewell(name) {
+    return `Goodbye, ${name}!`;
+}
+
+console.log(greet("Alice"));
+console.log(farewell("Bob"));
